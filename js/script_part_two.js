4a833f153a08d04b91bf59077254f169db01dce6
ReAlpha39 <yunusw718@gmail.com>
2020-12-18 22:39:03 +0800

    add PartTwo script files

---
+++ js/script_part_two.js
@@ -0,0 +1,11 @@
+monogatari.script({
+    // Part two
+    'PartTwo': [
+        "Sesampainya di persimpangan",
+        "kinan Kak, pergi dulu ya. bye - bye",
+        "arif Hati - hati ya dek.",
+        "Tidak lama kemudian, Roy teman sekelasku menghampiriku " +
+        "dengan motornya.",
+        "roy Hey Arif."
+    ]
+});
