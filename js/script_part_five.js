aa80eb53fa19a3a7fecadb66ad796a7879e7e020
ReAlpha39 <yunusw718@gmail.com>
2020-12-23 08:35:19 +0800

    fix unmatch voice lines with text lines

--- js/script_part_five.js
+++ js/script_part_five.js
@@ -26,56 +26,57 @@ monogatari.script({
         "nadia Dah siap, Saatnya berangkat.",
         "play voice 112",
         "nadia Bu, Nadia pergi dulu ya. mau ke mall bareng teman.",
+        //"play voice 113",
         "ibuNadia Hati hati ya nak.",
-        "play voice 113",
+        "play voice 114",
         "nadia Iya bu.",
         //sfx tutup pintu
         //Tidak lama kemudian nadia sampai ke tempat lokasi.
-        "play voice 114",
-        "nadia Mereka dimana ya?", //knpa ini ngga digabung aja?
         "play voice 115",
+        "nadia Mereka dimana ya?", //knpa ini ngga digabung aja?
+        "play voice 116",
         "nadia Kok ngga keliatan.",
         //Karena terlibat macet. Aku dan Roy pun datang terlambat.)
         "arif Maaf Nad telat, tadi kami kena macet di jalan Dipenegoro.",
-        "play voice 116",
-        "roy Iya nih. Lama bet soalnya.",
         "play voice 117",
+        "roy Iya nih. Lama bet soalnya.",
+        "play voice 118",
         "nadia Kalian sadar ga sih telat jam berapa?", //emang nunggu brpa lama?
         "arif Ya mau gimana lagi Nad, macet terus panas lagi.",
-        "play voice 118",
+        "play voice 119",
         "nadia Makanya jangan pake motor, itu yanmg buat suhu "+
         "di daerah kita naik gara gara polusi.",
-        "play voice 119",
-        "nadia Dah dibilangin tadi disekolah ga usah bawa motor, malah kabur.",
         "play voice 120",
+        "nadia Dah dibilangin tadi disekolah ga usah bawa motor, malah kabur.",
+        "play voice 121",
         "roy Eee.. iya Nad maaf, Aku ga bawa motor lagi deh baik "+
         "ke sekolah maupun ke luar jalan jalan.",
         "arif Hayolo dimarahin Nadia. Wahaha...",
-        "play voice 121",
+        "play voice 122",
         "roy Lah kamu kan bareng aku juga.",
         "arif Kan cuman numpang. hahahah...",
-        "play voice 122",
-        "nadia Awas ya kalo bawa motor.", //kok ngancem
         "play voice 123",
-        "roy Iya iya, Aku ga bawa.",
+        "nadia Awas ya kalo bawa motor.", //kok ngancem
         "play voice 124",
-        "nadia Oke jangan bawa motor untuk 3 bulan ya.", // siapa anda?
+        "roy Iya iya, Aku ga bawa.",
         "play voice 125",
+        "nadia Oke jangan bawa motor untuk 3 bulan ya.", // siapa anda?
+        "play voice 126",
         "roy Hah!?!?",
         "arif Hahaha..",
-        "play voice 126",
+        "play voice 127",
         "roy Lah kamu malah ketawa.",
         "arif Seru soalnya liatin kamu dimarahin sama cewek. Hahaha...",
-        "play voice 127",
-        "roy Teman macam apa kamu",
         "play voice 128",
-        "nadia Udah lah, ntar coba aja ga usah bawa motor 3 bulan. nanti tau sendiri efeknya.",
+        "roy Teman macam apa kamu",
         "play voice 129",
+        "nadia Udah lah, ntar coba aja ga usah bawa motor 3 bulan. nanti tau sendiri efeknya.",
+        "play voice 130",
         "roy Iya Nad.",
         "arif Yok lanjut kita ke mall. Ni waktu jalan terus loh.",
-        "play voice 130",
-        "roy Ayok.",
         "play voice 131",
+        "roy Ayok.",
+        "play voice 132",
         "nadia Ok.",
         //(Aku dan teman teman ku menghabiskan waktu seharian di mall)
         //(Tak sadar hari sudah mulai gelap, kami pun pulang kerumah masing masing.)
